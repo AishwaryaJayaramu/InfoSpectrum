ff6caf05298386cce4e0bdf7adb3a030cb1b4dde
Aditi Athreya <aditi.r.athreya@gmail.com>
2023-04-06 14:42:20 -0600

    added stockprice method to app.py

--- Flask_app_for_PublicAPI/app.py
+++ Flask_app_for_PublicAPI/app.py
@@ -1,4 +1,5 @@
-from flask import Flask, Response, abort, jsonify
+from flask import Flask, Response, abort,request,render_template, jsonify
+import yfinance as yf
 import requests
 import json
 import jsonpickle
@@ -192,6 +193,44 @@ def location_scores(name):
         scores = requests.get(scores_url).json()
         data.append(merge(loc, scores))
     return Response(response=jsonpickle.encode(data), status=200, mimetype="application/json")
+
+# API Route for pulling the stock quote
+@app.route("/quote")
+def display_quote():
+	# get a stock ticker symbol from the query string
+	# default to AAPL
+	symbol = request.args.get('symbol', default="AAPL")
+
+	# pull the stock quote
+	quote = yf.Ticker(symbol)
+
+	#return the object via the HTTP Response
+	print(jsonify(quote.info))
+	return jsonify(quote.info)
+
+# API route for pulling the stock history
+@app.route("/history")
+def display_history():
+	#get the query string parameters
+	symbol = request.args.get('symbol', default="AAPL")
+	period = request.args.get('period', default="1y")
+	interval = request.args.get('interval', default="1d")
+
+	#pull the quote
+	quote = yf.Ticker(symbol)	
+	#use the quote to pull the historical data from Yahoo finance
+	hist = quote.history(period=period, interval=interval)
+	#convert the historical data to JSON
+	data = hist.to_json()
+	#return the JSON in the HTTP response
+	return data
+
+# This is the / route, or the main landing page route.
+@app.route("/")
+def home():
+	# we will use Flask's render_template method to render a website template.
+    return render_template("homepage.html")
+
 
 
 if __name__ == '__main__':
