40aeb259831b95c011b8dfaaa6b00330ac95899b
Ruchi <rudh9897@colorado.edu>
2023-04-15 20:42:15 -0600

    frontend changes

---
+++ src/Company/layoff.js
@@ -0,0 +1,28 @@
+// import React from 'react';
+// import './citydetails.css'
+
+// function cityDetails(tbodyData) {
+//    const theadData = Object.keys(tbodyData[0]);
+//    return (
+//    <table>
+//       <thead>
+//          <tr>
+//       {theadData.map(heading => {
+//          return <th key={heading}>{heading}</th>
+//       })}
+//       </tr>
+//       </thead>
+//       <tbody>
+//       {tbodyData.map((row, index) => {
+//       return <tr key={index}>
+//       {theadData.map((key, index) => {
+//       return <td key={index}>{row[key]}</td>
+//       })}
+//       </tr>;
+//       })}
+//       </tbody>
+//    </table>
+//    );
+// }
+
+// export default cityDetails;
