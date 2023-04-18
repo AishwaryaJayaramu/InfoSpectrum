3c6464139fa78ba5e4cd41473e2f7bba141b9561
SudarshanSridhar <sudarshan.sridhar@colorado.edu>
2023-04-17 23:04:24 -0600

    Adding the URLs of major tech company reviews

---
+++ Flask_app_for_PublicAPI/UrlConstants.py
@@ -0,0 +1,22 @@
+Amazon_review_url = "https://www.glassdoor.com/Reviews/Amazon-Reviews-E6036.htm"
+
+Glassdoor_review_url = "https://www.glassdoor.com/Reviews/Glassdoor-Reviews-E100431.htm"
+
+Meta_review_url = "https://www.glassdoor.com/Reviews/Meta-Reviews-E40772.htm"
+
+Google_review_url = "https://www.glassdoor.com/Reviews/Google-Reviews-E9079.htm"
+
+Oracle_review_url = "https://www.glassdoor.com/Reviews/Oracle-Reviews-E1737.htm"
+
+Microsoft_review_url = "https://www.glassdoor.com/Reviews/Microsoft-Reviews-E1651.htm"
+
+Apple_review_url = "https://www.glassdoor.com/Reviews/Apple-Reviews-E1138.htm"
+
+Twitter_review_url = "https://www.glassdoor.com/Reviews/Twitter-Reviews-E100569.htm"
+
+Airbnb_review_url = "https://www.glassdoor.com/Reviews/Airbnb-Reviews-E391850.htm"
+
+Snap_review_url = "https://www.glassdoor.com/Reviews/Snap-Reviews-E671946.htm"
+
+Uber_review_url = "https://www.glassdoor.com/Reviews/Uber-Reviews-E575263.htm"
+
